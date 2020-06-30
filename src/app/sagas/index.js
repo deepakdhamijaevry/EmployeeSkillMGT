0ae30773b765c898f8308827a57562442d442096
deepakdhamijaevry <deepak.dhamija@evry.com>
2020-06-30 13:19:50 +0530

    Saga Started with employee

--- src/app/sagas/index.js
+++ src/app/sagas/index.js
@@ -1,9 +1,11 @@
-import { LOGIN_REQUEST } from '../actions/actionTypes';
+import { LOGIN_REQUEST, GET_EMPLOYEES_REQUEST } from '../actions/actionTypes';
 import { takeLatest, all } from 'redux-saga/effects';
 import { login } from '../actions/loginAction'
+import { getEmployees } from '../actions/employeeAction'
 
 function* actionWatcher() {
   yield takeLatest(LOGIN_REQUEST, login)
+  yield takeLatest(GET_EMPLOYEES_REQUEST, getEmployees)
 }
 
 export default function* rootSaga() {
